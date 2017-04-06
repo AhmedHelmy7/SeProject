80a0004b317443791594cd1a8b4b3f1205f26a34
david123onsy <davidonsy@gmail.com>
2017-04-07 00:15:09 +0200

    Omar items

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var router = require('./routes/routes');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var DB_URI = "mongodb://localhost:27017/finalProject";
-var session = require('express-session');
+
 var app = express();
 
 // configure app
@@ -13,7 +13,6 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
-app.use(session({ secret: "ronaldo", resave: false, saveUninitialized: true }));
 
 mongoose.connect(DB_URI, function(err) {
     if (err) {
@@ -25,6 +24,6 @@ mongoose.connect(DB_URI, function(err) {
 app.use('/', router);
 
 // start the server
-app.listen(8091, function() {
-    console.log("server is listening on port 8091");
+app.listen(8095, function() {
+    console.log("server is listening on port 8095");
 })
