c1a958b9ca534e9c62977ec8a9a6b478e7ea3777
Ahmed Helmy <helmyy1996@gmail.com>
2017-04-07 01:22:55 +0200

    updating

--- server.js
+++ server.js
@@ -27,3 +27,51 @@ app.use('/', router);
 app.listen(8095, function() {
     console.log("server is listening on port 8095");
 })
+const express=require('express');
+const path=require('path');
+const bodyParser = require('body-parser');
+const cors=require('cors');
+const passport=require('passport');
+const mongoose=require('mongoose');
+const morgan=require('morgan');
+var session=require('express-session');
+
+mongoose.connect('mongodb://localhost:27017/finalProject',function(err)
+{
+    if(err)
+    {
+        console.log('not connected')
+    }
+    else{
+        
+        console.log('success on db connection');
+    }
+});
+
+const app=express();
+const users=require('../SeProject/app/routes/userRoutes');
+const port=8089;
+
+
+//app.use('./users',users);
+
+app.use(cors());
+app.use(express.static(__dirname + '/public'));
+app.use(session({secret:"ronaldo",resave:false,saveUninitialized:true}));
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(passport.initialize());
+app.use(passport.session());
+require('./config/passport')(passport);
+app.use('/users',users);
+
+
+app.get('/',function(req,res)
+{
+});
+app.listen(port,function()
+{
+    console.log('Running on port '+port);
+});
+//27017q
