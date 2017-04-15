752112efe3a48531fc63b571c04ffcc2e393c46e
Ahmed Helmy <helmyy1996@gmail.com>
2017-04-15 14:38:38 +0200

    Updating david

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-<<<<<<< HEAD
-=======
 var express = require('express');
-var router = require('./routes/routes');
+var router = require('/home/helmy/Desktop/SeProject/app/routes/routes.js');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var DB_URI = "mongodb://localhost:27017/finalProject";
 
 var app = express();
+const port=8097;
 
 // configure app
 app.use(bodyParser.urlencoded({
@@ -25,36 +24,13 @@ mongoose.connect(DB_URI, function(err) {
 });
 app.use('/', router);
 
-// start the server
-app.listen(8095, function() {
-    console.log("server is listening on port 8095");
-})
->>>>>>> Omar
-const express=require('express');
 const path=require('path');
-const bodyParser = require('body-parser');
 const cors=require('cors');
 const passport=require('passport');
-const mongoose=require('mongoose');
 const morgan=require('morgan');
 var session=require('express-session');
 
-mongoose.connect('mongodb://localhost:27017/finalProject',function(err)
-{
-    if(err)
-    {
-        console.log('not connected')
-    }
-    else{
-        
-        console.log('success on db connection');
-    }
-});
-
-const app=express();
 const users=require('../SeProject/app/routes/userRoutes');
-const port=8089;
-
 
 //app.use('./users',users);
 
